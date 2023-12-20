df86bfba173430d0e4a67562ce8d9d51d1e4b3b1
Giacomo Pignoni <giacomo.pignoni@gmail.com>
2023-12-20 10:23:49 +0100

    [clay] bump engine 3.22.1

--- packages/flutter_tools/lib/src/build_system/targets/macos.dart
+++ packages/flutter_tools/lib/src/build_system/targets/macos.dart
@@ -439,6 +439,7 @@ abstract class MacOSBundleFlutterAssets extends Target {
       environment,
       assetDirectory,
       targetPlatform: TargetPlatform.darwin,
+      shaderTarget: ShaderTarget.impelleriOS,
       flavor: environment.defines[kFlavor],
     );
     environment.depFileService.writeToFile(
