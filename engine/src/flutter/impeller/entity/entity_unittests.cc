93e509079e228940861e09f5870ca4da84c447ee
Giacomo Pignoni <giacomo.pignoni@gmail.com>
2025-10-15 10:35:05 +0200

    fix test

--- engine/src/flutter/impeller/entity/entity_unittests.cc
+++ engine/src/flutter/impeller/entity/entity_unittests.cc
@@ -2626,6 +2626,11 @@ class FlushTestAllocator : public Allocator {
       const TextureDescriptor& desc) override {
     return nullptr;
   }
+
+  std::shared_ptr<Texture> WrapTexture(const TextureDescriptor& desc,
+                                       int64_t raw_texture) const {
+    return nullptr;
+  }
 };
 
 class FlushTestContentContext : public ContentContext {
